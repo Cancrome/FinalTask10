a12a87a8e2e9e831045a4507c00e3e8bf893716c
Mary <marysok-16@mail.ru>
2023-03-27 20:41:40 +0600

    solution task5

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -7,3 +7,26 @@
 Подсказки:
 --- используйте модуль chardet, иначе задание не засчитается!!!
 """
+
+import subprocess
+import chardet
+
+link1 = 'yandex.ru'
+ARGS = ['ping', link1]
+YA_PING = subprocess.Popen(ARGS, stdout=subprocess.PIPE)
+print('YANDEX: ')
+for line in YA_PING.stdout:
+    result = chardet.detect(line)
+    line = line.decode(result['encoding']).encode('utf-8')
+    print(line.decode('utf-8'))
+
+
+print('_' * 20)
+print('YOUTUBE: ')
+link2 = 'youtube.com'
+ARGS = ['ping', link2]
+YA_PING = subprocess.Popen(ARGS, stdout=subprocess.PIPE)
+for line in YA_PING.stdout:
+    result = chardet.detect(line)
+    line = line.decode(result['encoding']).encode('utf-8')
+    print(line.decode('utf-8'))
